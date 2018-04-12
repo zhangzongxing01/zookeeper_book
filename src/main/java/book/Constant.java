9f5e5d82ce0c3263bf7cb3776586e4a10693c7e8
hzzhangzongxing <hzzhangzongxing@corp.netease.com>
2018-04-12 20:12:54 +0800

    @misc

---
+++ src/main/java/book/Constant.java
@@ -0,0 +1,11 @@
+package book;
+
+/**
+ * @author hzzhangzongxing
+ * @version 2018/4/12
+ */
+public class Constant {
+    public static String ZK_SERVER_ADD="hzbxs-study-test024.server.163.org:2181," +
+            "hzbxs-study-test025.server.163.org:2181," +
+            "hzbxs-study-test058.server.163.org:2181";
+}
